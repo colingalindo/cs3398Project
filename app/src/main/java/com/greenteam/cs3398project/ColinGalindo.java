95d16305a671d52e528870ec07ee6ca74d46ed1d
Colin Galindo <galindo.colin@gmail.com>
2014-09-24 13:41:02 -0500

    Commenting out in hopes of fixing the automated build

--- app/src/main/java/com/greenteam/cs3398project/ColinGalindo.java
+++ app/src/main/java/com/greenteam/cs3398project/ColinGalindo.java
@@ -1,1 +1,1 @@
-This is a test file and should be deleted at some point in the near future or else the world will explode in to a mile pieces
+//This is a test file and should be deleted at some point in the near future or else the world will explode in to a mile pieces
