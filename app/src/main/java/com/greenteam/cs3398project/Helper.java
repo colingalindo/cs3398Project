7e8d7ab858d667fa36bb3f10a03af5bac30f48db
sampehr <sep73@txstate.edu>
2014-10-27 11:43:20 -0500

    Restful API user-side
    implemented basic skeleton of helper file, filled with dummy functions

---
+++ app/src/main/java/com/greenteam/cs3398project/Helper.java
@@ -0,0 +1,99 @@
+package com.greenteam.cs3398project;
+
+/**
+ * Created by sam on 10/27/2014.
+ */
+public class Helper {
+
+    public String GetUsername(){
+
+        String dummy_value = "dummy_user";
+        return dummy_value;
+    }
+
+    public boolean SendUsername(String username){
+        //return 1 if successful, 0 if unsuccessful
+        boolean dummy_value = true;
+        return dummy_value;
+
+    }
+
+    public boolean ValidatePassword(String password){
+        //return 1 if successful, 0 if unsuccessful
+        String encrypted_password = encrypt(password);
+
+        boolean dummy_value = true;
+        return dummy_value;
+
+    }
+
+    public String GetFirstName(){
+
+        String dummy_value = "dummy_name";
+        return dummy_value;
+    }
+
+    public boolean SendFirstName(String firstName){
+        //return 1 if successful, 0 if unsuccessful
+        boolean dummy_value = true;
+        return dummy_value;
+
+    }
+
+    public String GetLastName(){
+
+        String dummy_value = "dummy_LastName";
+        return dummy_value;
+    }
+
+    public boolean SendLastName(String LastName){
+        //return 1 if successful, 0 if unsuccessful
+        boolean dummy_value = true;
+        return dummy_value;
+
+    }
+
+    public String GetEMail(){
+
+        String dummy_value = "dummy_EMail";
+        return dummy_value;
+    }
+
+    public boolean SendEMail(String EMail){
+        //return 1 if successful, 0 if unsuccessful
+        boolean dummy_value = true;
+        return dummy_value;
+
+    }
+
+    public String GetWorkNumber(){
+
+        String dummy_value = "dummy_WorkNumber";
+        return dummy_value;
+    }
+
+    public boolean SendWorkNumber(String WorkNumber){
+        //return 1 if successful, 0 if unsuccessful
+        boolean dummy_value = true;
+        return dummy_value;
+
+    }
+
+    public String GetMobileNumber(){
+
+        String dummy_value = "dummy_MobileNumber";
+        return dummy_value;
+    }
+
+    public boolean SendMobileNumber(String MobileNumber){
+        //return 1 if successful, 0 if unsuccessful
+        boolean dummy_value = true;
+        return dummy_value;
+
+    }
+
+    public String encrypt(String password){
+        String dummy_value = password;
+        return dummy_value;
+    }
+}
